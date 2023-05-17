0b0675ebd26f765aacf724df25bad37763b667c4
anacatarinafg <anafrontenddeveloper@gmail.com>
2023-05-18 00:39:10 +0100

    adding plus, minus and remove icons and their functionality

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,10 +2,11 @@ import React, { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './cart.css';
 import { contextForCart } from '../../context/contextForCart';
+import { UpIcon, HomeIcon, DownIcon, CartIcon, TrashIcon } from "../Icons";
 
 const Cart = () => {
   const navigate = useNavigate();
-  const { getItems } = useContext(contextForCart);
+  const { getItems, clearProducts, increaseQuantity, decreaseQuantity, removeProduct } = useContext(contextForCart);
 
   const renderCart = () => {
     const cartItems = getItems();
@@ -15,7 +16,12 @@ const Cart = () => {
           <div>
             <Link to={`/products/${item.id}`} className='cart__link'>{item.title}</Link>
           </div>
-          <h4 className='cart__quantity'>{item.quantity}</h4>
+          <div className='cart__quantityDiv'>
+            <h4 className='cart__quantity'>{item.quantity}</h4>
+            <UpIcon width={20} onClick={() => increaseQuantity({id: item.id}) } />
+            <DownIcon width={20} onClick={() => decreaseQuantity({id: item.id}) } />
+            <TrashIcon width={20} onClick={() => removeProduct({id: item.id}) } />
+          </div>
           <h4 className='cart__price'>{item.price}€</h4>
         </React.Fragment>
       ));
@@ -26,7 +32,7 @@ const Cart = () => {
   return (
     <div className='cart'>
       <h3 className='cart__headline'>Shopping cart</h3>
-      <button className='cart__button'>Checkout</button>
+      <button className='cart__button' onClick={() => navigate("/checkout")}>Checkout</button>
       <div className='cart__products'>
         <div className='cart__titles'>
           <h4 className='cart__title'>Item</h4>
@@ -38,7 +44,7 @@ const Cart = () => {
           {renderCart()}
         </div>
         {/* ADD UNDERLINE */}
-        <button className='cart__button cart__clear'>Clear</button>
+        <button className='cart__button cart__clear' onClick={() => clearProducts()}>Clear</button>
         <p className='cart__total'>Total: </p>
       </div>
     </div>
